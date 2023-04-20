7c370ef72eb8d4b7f7ba565f81d2bd7abe704601
Asger Ougaard <asger.ougaard@wsa.com>
2023-04-20 21:07:44 +0200

    set default rating in ts_setup

--- src/widgets/utils/ts_setup.py
+++ src/widgets/utils/ts_setup.py
@@ -1,10 +1,14 @@
 import trueskill as ts
 
 # Set parameters for trueskill calculations
-SIGMA = ts.SIGMA # Default sigma value
-BETA = SIGMA # Increase beta to make initial ratings less volatile
-TAU = ts.SIGMA / 20 # Increase tau to prevent ratings getting "stuck" after a number of matches
-DRAW_PROBABILITY = 0.0 # Not possible to draw
+SIGMA = ts.SIGMA  # Default sigma value
+BETA = SIGMA  # Increase beta to make initial ratings less volatile
+TAU = (
+    ts.SIGMA / 20
+)  # Increase tau to prevent ratings getting "stuck" after a number of matches
+DRAW_PROBABILITY = 0.0  # Not possible to draw
+MU = 25.0  # Default TrueSkill rating
+
 
 def ts_setup():
     ts.setup(
